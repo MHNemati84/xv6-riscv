4cd382b192b46c8a32bb4254bfd759f61194ac1e
mh-nemati <mohnemati84@gmail.com>
2025-03-26 11:31:26 +0330

    Added a Message Logger

--- kernel/main.c
+++ kernel/main.c
@@ -1,8 +1,10 @@
+#include "system_logger.h"
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
 #include "riscv.h"
 #include "defs.h"
+
 
 volatile static int started = 0;
 
@@ -30,12 +32,21 @@ main()
     virtio_disk_init(); // emulated hard disk
     userinit();      // first user process
     __sync_synchronize();
+    
+    logger_init();
+    logger_message(INFO,
+      "Welcome to AUT MCS Principles of Operating System Course.This message is from a custom logger implemented by 40212048 and 40213437");
+    logger_message(WARNING,"This is a test warning message for the custom logger");
+    logger_message(ERROR,"This is a test error message for the custom logger.");
+    printf("\n");
+
     started = 1;
   } else {
     while(started == 0)
       ;
     __sync_synchronize();
     printf("hart %d starting\n", cpuid());
+    
     kvminithart();    // turn on paging
     trapinithart();   // install kernel trap vector
     plicinithart();   // ask PLIC for device interrupts
